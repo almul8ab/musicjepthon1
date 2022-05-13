6706a5473e688cf32788396cee0fd8e0669799ae
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2022-05-13 08:31:14 -0700

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -45,7 +45,7 @@ async def play(_, message: Message):
     await message.delete()
     # I Can See You !!
     do = requests.get(
-        f"https://api.telegram.org/bot5354438194:AAEgUy1aY9HLf29CUUuSxdZJP4r4vFPv9W4/getChatMember?chat_id=@jepthon&user_id={message.from_user.id}").text
+        f"https://api.telegram.org/bot5354438194:AAHi1B4DLYS21Y-t0hbmmBAzGeT_MeIDLuw/getChatMember?chat_id=@jepthon&user_id={message.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
         keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/jepthon')]]
         reply_markup03 = InlineKeyboardMarkup(keyboard03)
