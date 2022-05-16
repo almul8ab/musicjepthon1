57fcae0f1f52b1da1642e4271f83f37e674f74a2
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2022-05-16 20:18:33 +0300

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -97,13 +97,6 @@ async def welcome(_, message: Message):
 @app.on_message(filters.command(["help", "start"]) & filters.group)
 @PermissionCheck
 async def useradd(_, message: Message):
-    # I Can See You !!
-    do = requests.get(f"https://api.telegram.org/bot5354438194:AAHi1B4DLYS21Y-t0hbmmBAzGeT_MeIDLuw/getChatMember?chat_id=@jepthon&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك.", url='https://t.me/jepthon')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('-عذࢪأ ، عليك الاشتࢪاك في قناة البوت اولا .',
-                                 reply_markup=reply_markup03)
     else:
         out = start_pannel()
         await asyncio.gather(
